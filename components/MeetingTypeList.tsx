4918bc7b454b6062989c4d85cf2044dbdc30e6d4
Jeremy Twidt <jtwidt@gmail.com>
2024-03-30 19:04:44 -0500

    new meeting modal created

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,12 +4,15 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import HomeCard from './HomeCard';
 import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
   >();
+
+  const createMeeting = () => {};
 
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
@@ -40,6 +43,14 @@ const MeetingTypeList = () => {
         description='Check out your recordings'
         handleClick={() => router.push('/recordings')}
         className='bg-yellow-1'
+      />
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title='Start an instant meeting'
+        className='text-center'
+        buttonText='Start Meeting'
+        handleClick={createMeeting}
       />
     </section>
   );
