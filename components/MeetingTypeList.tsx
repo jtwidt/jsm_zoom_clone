a8b4bae6725013040032d063b6c8150739c1d7e3
Jeremy Twidt <jtwidt@gmail.com>
2024-04-02 21:50:26 -0500

    completed home page buttons

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -11,6 +11,7 @@ import ReactDatePicker from 'react-datepicker';
 
 import { useToast } from '@/components/ui/use-toast';
 import { Textarea } from './ui/textarea';
+import { Input } from './ui/input';
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -163,6 +164,20 @@ const MeetingTypeList = () => {
         buttonText='Start Meeting'
         handleClick={createMeeting}
       />
+      <MeetingModal
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title='Type the link here'
+        className='text-center'
+        buttonText='Join Meeting'
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder='Meeting link'
+          className='border-none bg-dark-3 focus-visible:ring-0 focus.visible:ring-offest-0'
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+        />
+      </MeetingModal>
     </section>
   );
 };
